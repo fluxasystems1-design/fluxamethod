6bd519d1532bb44fdd1e8f721fe4e5bc0f928498
fluxasystems1-design <fluxasystems1@gmail.com>
2026-04-14 20:12:55 -0500

    agregar meta pixel y eventos diagnostico

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,31 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href={fontHref} rel="stylesheet" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1007002315008094');
+      fbq('track', 'PageView');
+    `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1007002315008094&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
